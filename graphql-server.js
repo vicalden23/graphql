9625bf779cb44fb8060dc532bb0346b11951e3ec
vicki alden <vic.alden23@gmail.com>
2017-08-16 12:04:31 -0700

    Add graphql-express server

---
+++ graphql-server.js
@@ -0,0 +1,33 @@
+var express = require('express');
+var graphqlHTTP = require('express-graphql');
+var { buildSchema } = require('graphql');
+
+var schema = buildSchema (`
+  type Query {
+    quoteOfTheDay: String
+    random: Float
+    rollThreeDice: [Int]
+  }
+`);
+
+var root = {
+  quoteOfTheDay: () => {
+    return Math.random() < 0.5 ? 'Take it easy' : 'Salvation lies within';
+  },
+  random: () => {
+    return Math.random();
+  },
+  rollThreeDice: () => {
+    return [1, 2, 3].map(_ => 1 + Math.floor(Math.random() * 6));
+  },
+};
+
+var app = express();
+app.use('/graphql', graphqlHTTP({
+  schema: schema,
+  rootValue: root,
+  graphiql: true,
+}));
+
+app.listen(4000);
+console.log('Running a GraphQL API server at localhost:4000/graphql');
